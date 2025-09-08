44c9a494272648208e1efa4fccce8f6613e20ae4
Marouane Touzani <marouane1touzani@gmail.com>
2025-09-08 18:55:06 +0100

    Refactor event handling in script.js to improve option button interaction and display explanations

--- 3_quiz_game/script.js
+++ 3_quiz_game/script.js
@@ -39,30 +39,36 @@
     // TODO 5: Declare an isCorrect function that compares a guess to the right answer
     // isCorrect(guess) should return true if the guess matches the fact's answer
     function isCorrect(guess){
-        return guess === fact.answer;
+        return guess == fact.answer;
     }
 
 
     // TODO 6A: Use a for loop to add a click event listener to each of the optionButtons
     for (let button of optionButtons){
-                button.addEventListener("click", (event) => {
-                   
-                     
-                    // TODO 6B: Within the event handler function, display the fact's explanation by setting the text of the explanation element
-                    
-                    explanation.textContent = fact.explanation;
-                    // TODO 7: Within the event handler function, 
-                    // Use a for loop to disable all the option buttons
-                    for (let button of optionButtons){
-                        disable(button);
-                    }
-                    
-                    // TODO 8: Within the event handler function,
-                    // Get the guessed value from the clicked button
-                    // Use a conditional to compare the guess to the fact's answer
-                    // and add the "correct"/"incorrect" class as appropriate
-                })
+        button.addEventListener("click", (event) => {
+
+            // show explanation
+            explanation.textContent = fact.explanation;
+
+            // disable all option buttons
+            for (let opt of optionButtons){
+                disable(opt);
             }
+
+            // Get the clicked button (use currentTarget to avoid issues if nested elements exist)
+            const clicked = event.currentTarget;
+            // button values are strings "true"/"false" in the HTML; coerce to boolean
+            const guess = clicked.value === "true"
+
+            // add appropriate class on the clicked button
+            if (isCorrect(guess)){
+                clicked.classList.add("correct");
+            }
+            else{
+                clicked.classList.add("incorrect");
+            }
+        })
+    }
 
     
 
